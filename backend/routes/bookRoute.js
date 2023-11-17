53d9a60409bfbb254c783fc57fefa3f580ab1511
Chethan L <chethan.shantha@gmail.com>
2023-11-17 22:53:46 +0530

    added patch request for the book route

--- backend/routes/bookRoute.js
+++ backend/routes/bookRoute.js
@@ -19,13 +19,13 @@ bookroute.get("/:id", async (request, response) => {
         const res = await book.findById(id);
         if (!res || res == {}) {
             return response.status(404).send({
-                messege: "Could not find the record with id.",
+                messege: `Could not find the record with id : ${id}`,
             });
         }
         return response.status(200).json(res);
     } catch (error) {
         return response.status(500).send({
-            message: "Failed to get id",
+            message: "something went wrong",
             ereor: error,
         });
     }
@@ -49,8 +49,8 @@ bookroute.post("/", async (request, response) => {
             messege: "Entry created succesfully",
         });
     } catch (error) {
-        return response.status(400).send({
-            messege: "Failed to create the recort",
+        return response.status(500).send({
+            messege: "Failed to create the record",
             error: error,
         });
     }
@@ -71,4 +71,45 @@ bookroute.delete("/:id", async (request, response) => {
     }
 });
 
+bookroute.patch("/:id", async (request, response) => {
+    let id = request.params.id;
+    try {
+        let res = book.findById(id);
+        if (!res || res == {}) {
+            return response.status(404).send({
+                messege: `Could not find the record with id : ${id}`,
+            });
+        } else {
+            let { title, author, year, publisher } = request.body;
+            if (!title || !year || !publisher || !author) {
+                return response.status(400).send({
+                    message:
+                        "provide all the values : title , year, author, publisher",
+                });
+            }
+            const record = await book.findOneAndUpdate(
+                { _id: id },
+                {
+                    title: title,
+                    author: author,
+                    year: year,
+                    publisher: publisher,
+                }
+            );
+            let updatedRecord = await book.findById(id);
+            return response.status(201).send({
+                message : `Succesfully updated the record with id : ${id}`,
+                data : {
+                old: record,
+                new: updatedRecord
+                }
+            });
+        }
+    } catch (error) {
+        return response.status(500).send({
+            messege: `Something went wrong in the server`,
+            error: error,
+        });
+    }
+});
 module.exports = bookroute;
